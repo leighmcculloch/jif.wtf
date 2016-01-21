e8269a3614cfbab65c5d3907cb95191f26d4afbf
Leigh McCulloch <leigh@mcchouse.com>
2016-01-20 21:10:21 -0800

    Disabled s3_sync after build.
    What
    ===
    Disabled s3_sync after build. It must be run manually.
    
    Why
    ===
    There's a bug in something and s3_sync after build isn't uploading all
    the files that changed.X

--- website/config.rb
+++ website/config.rb
@@ -6,7 +6,7 @@ activate :s3_sync do |s3_sync|
   s3_sync.bucket                     = 'jif.wtf'
   s3_sync.region                     = 'us-east-1'
   s3_sync.delete                     = false
-  s3_sync.after_build                = true
+  s3_sync.after_build                = false
   s3_sync.prefer_gzip                = true
   s3_sync.path_style                 = true
 end
