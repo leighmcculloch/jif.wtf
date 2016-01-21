af5a3fc926552085c9884677c602937fd132a9f0
Leigh McCulloch <leigh@mcchouse.com>
2016-01-20 19:48:47 -0800

    Moved code into multiple files.
    What
    ===
    Moved code for the different types into separate files.
    
    Why
    ===
    Sanity

--- service/src/jifwtfservice/main.go
+++ service/src/jifwtfservice/main.go
@@ -1,17 +1,23 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
-	"net/url"
 	"os"
 	"sort"
 
 	"github.com/googollee/go-socket.io"
 )
+
+func onConnection(so socketio.Socket) {
+	so.On("search", func(query string) {
+		results := GetImgurResults(query)
+		sort.Sort(SearchResultsByRating(results))
+
+		so.Emit("results", query, results)
+	})
+}
 
 func main() {
 	server, err := socketio.NewServer(nil)
@@ -21,14 +27,7 @@ func main() {
 
 	server.SetCookie("JSESSIONID")
 
-	server.On("connection", func(so socketio.Socket) {
-		so.On("search", func(query string) {
-			results := getImgurResults(query)
-			sort.Sort(ByRating(results))
-
-			so.Emit("results", query, results)
-		})
-	})
+	server.On("connection", onConnection)
 
 	http.HandleFunc("/socket.io/", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "http://jif.wtf")
@@ -46,78 +45,4 @@ func main() {
 	var address string = fmt.Sprintf("%s:%s", host, port)
 	log.Println("Listening on", address)
 	log.Fatal(http.ListenAndServe(address, nil))
-}
-
-type SearchResult struct {
-	Width  int    `json:"width"`
-	Height int    `json:"height"`
-	Mp4    string `json:"mp4"`
-	Gif    string `json:"gif"`
-	Rating int    `json:"rating"`
-}
-
-type ByRating []SearchResult
-
-func (s ByRating) Len() int {
-	return len(s)
-}
-func (s ByRating) Swap(i, j int) {
-	s[i], s[j] = s[j], s[i]
-}
-func (s ByRating) Less(i, j int) bool {
-	return s[i].Rating < s[j].Rating
-}
-
-func getImgurResults(search string) []SearchResult {
-	params := url.Values{}
-	params.Add("q_type", "anigif")
-	params.Add("q_all", search)
-
-	url := url.URL{
-		Scheme:   "https",
-		Host:     "api.imgur.com",
-		Path:     "/3/gallery/search",
-		RawQuery: params.Encode(),
-	}
-
-	client := &http.Client{}
-	req, _ := http.NewRequest("GET", url.String(), nil)
-	req.Header.Set("Authorization", "Client-ID a27edb3db737edf")
-	res, err := client.Do(req)
-	if err != nil {
-		log.Fatal("Imgur API Error:", err)
-	}
-
-	body, _ := ioutil.ReadAll(res.Body)
-
-	type ImgurSearchResult struct {
-		Width     int    `json:"width"`
-		Height    int    `json:"height"`
-		Mp4       string `json:"mp4"`
-		Gifv      string `json:"gifv"`
-		Gif       string `json:"link"`
-		IsLooping bool   `json:"looping"`
-		Points    int    `json:"points"`
-	}
-
-	type ImgurSearchResponse struct {
-		Success bool                `json:"success"`
-		Results []ImgurSearchResult `json:"data"`
-	}
-
-	var searchResponse ImgurSearchResponse
-	json.Unmarshal(body, &searchResponse)
-
-	results := make([]SearchResult, len(searchResponse.Results))
-	for i := 0; i < len(searchResponse.Results); i++ {
-		results[i] = SearchResult{
-			Width:  searchResponse.Results[i].Width,
-			Height: searchResponse.Results[i].Height,
-			Mp4:    searchResponse.Results[i].Mp4,
-			Gif:    searchResponse.Results[i].Gif,
-			Rating: searchResponse.Results[i].Points,
-		}
-	}
-
-	return results
 }
---
+++ service/src/jifwtfservice/search_result.go
@@ -0,0 +1,23 @@
+package main
+
+type SearchResult struct {
+	Width  int    `json:"width"`
+	Height int    `json:"height"`
+	Mp4    string `json:"mp4"`
+	Gif    string `json:"gif"`
+	Rating int    `json:"rating"`
+}
+
+type SearchResultsByRating []SearchResult
+
+func (s SearchResultsByRating) Len() int {
+	return len(s)
+}
+
+func (s SearchResultsByRating) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+func (s SearchResultsByRating) Less(i, j int) bool {
+	return s[i].Rating > s[j].Rating
+}
