480ba1b892bdae767f3004e5aedb3ec100502570
Leigh McCulloch <leigh@mcchouse.com>
2016-05-09 22:50:16 -0700

    Switched to hosting on Github.
    What
    ===
    Switched to hosting on Github.
    
    Why
    ===
    To remove the middleman-s3_sync dependency.

--- website/config.rb
+++ website/config.rb
@@ -1,12 +1,3 @@
 configure :build do
   activate :asset_hash
 end
-
-activate :s3_sync do |s3_sync|
-  s3_sync.bucket                     = 'jif.wtf'
-  s3_sync.region                     = 'us-east-1'
-  s3_sync.delete                     = false
-  s3_sync.after_build                = false
-  s3_sync.prefer_gzip                = true
-  s3_sync.path_style                 = true
-end
